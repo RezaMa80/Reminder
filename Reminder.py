09997f7171c85b9a533832076fb5e757a1eaab50
RezaMa80 <measgail@gmail.com>
2023-11-24 23:35:24 +0330

    Debug and add comment

--- Reminder.py
+++ Reminder.py
@@ -1,68 +1,93 @@
-#WORK_TIME=3600
-#SNOOZE_TIME=300
-#BREAK_TIME=300
-#AFK_THRESHOLD=60
-
-
 from tkinter import Tk, ttk
 from pynput import mouse, keyboard
 
-BREAKING = 1
-WORKING = 2
+# Extract value of the given key from data of config file
+def readConfig(key):
+    startIndex = configData.index(key) + len(key) + 1
 
-def readConfig(config):
-    startIndex = configData.index(config) + len(config) + 1
     try:
         endIndex = configData.index('\n', startIndex)
+        # Last line doesn't have \n so there will be an error
     except ValueError:
         endIndex = len(configData)
 
     return int(configData[startIndex:endIndex])
 
+# Write the given text to 'Data' file
 def writeWindowStatus(text):
     F_data = open('Data', 'w')
     F_data.write(text)
     F_data.close()
 
+# Read the text from given file
+def readWindowStatus(file = 'Data'):
+    F_data = open(file)
+    windowStatus = F_data.read()
+    F_data.close()
+
+    return windowStatus
+
+# Close program
 def stop_C():
     writeWindowStatus('Open')
-    exit()
+    root.destroy()
 
+# Hide window
 def hide_C():
     root.withdraw()
-    writeWindowStatus('Show')
+    writeWindowStatus('Nothing')
 
-def onClosing():
-    writeWindowStatus('Open')
-
-def secondToHour(number):
+# Return minute:second format of the given seconds
+def toClock(number):
     number = int(number)
-    return str(number//60) + ':' + str(number%60)
+    hour = str(number//60)
+    second = str(number%60)
 
+    if len(hour) < 2:
+        hour = '0' + hour
+
+    if len(second) < 2:
+        second = '0' + second
+
+    return hour + ':' + second
+
+# Unhide window and bring it to top
 def popUp():
     root.deiconify()
     root.attributes('-topmost', True)
     root.attributes('-topmost', False)
 
+# Reset break time
+def action(arg1 = '', arg2 = '', arg3 = '', arg4 = ''):
+    global breakTime
+    breakTime = 0
+
+# Trig every 1 second
 def clock():
     global second, breakTime, goal
     breakTime += 1
 
+    # Breaking
     if breakTime >= AFK_THRESHOLD:
         if breakTime == AFK_THRESHOLD:
+            popUp()
             second = min(second, WORK_TIME)
 
             goal = second//(WORK_TIME/BREAK_TIME)
-            second -= AFK_THRESHOLD//(WORK_TIME/BREAK_TIME)
+            second -= AFK_THRESHOLD*(WORK_TIME//BREAK_TIME)
         elif second - WORK_TIME//BREAK_TIME >= 0:
             second -= WORK_TIME//BREAK_TIME
         
         second = max(second, 0)
 
-        L_status.config(text = 'Break for ' + secondToHour(goal))
-        L_time.config(text = secondToHour(breakTime))
+        L_status.config(text = 'Break for ' + toClock(goal))
+        L_time.config(text = toClock(breakTime))
+
+    # Working
     else:
+        # Over working
         if second >= WORK_TIME:
+            # Check snooze
             if second == WORK_TIME or (second - WORK_TIME)%SNOOZE_TIME == 0:
                 L_status.config(text = 'You should take a break')
                 popUp()
@@ -70,35 +95,28 @@ def clock():
             L_status.config(text = 'You\'re working')
 
         second += 1
-        L_time.config(text = secondToHour(second))
+        L_time.config(text = toClock(second))
+
+    if readWindowStatus() == 'Show':
+        popUp()
+        writeWindowStatus('Nothing')
 
     root.after(1000, clock)
 
-def action(arg1 = '', arg2 = '', arg3 = '', arg4 = ''):
-    global breakTime
-    breakTime = 0
+if readWindowStatus() == 'Open':
+    writeWindowStatus('Nothing')
 
-F_data = open('Data')
-windowStatus = F_data.read()
-F_data.close()
-
-mouse.Listener(on_move = action, on_click = action, on_scroll = action).start()
-keyboard.Listener(on_press = action, on_release = action).start()
-
-if windowStatus == 'Open':
-    #writeFile('Show')
-
-    F_config = open('Config.txt')
-    configData = F_config.read()
-
+    configData = readWindowStatus('Config.txt')
     WORK_TIME = readConfig('WORK_TIME')
     SNOOZE_TIME = readConfig('SNOOZE_TIME')
     BREAK_TIME = readConfig('BREAK_TIME')
     AFK_THRESHOLD = readConfig('AFK_THRESHOLD')
 
-    status = WORKING
     second = 0
     breakTime = 0
+
+    mouse.Listener(on_move = action, on_click = action, on_scroll = action).start()
+    keyboard.Listener(on_press = action, on_release = action).start()
 
     root = Tk()
 
@@ -115,7 +133,7 @@ if windowStatus == 'Open':
     B_Hide.pack()
 
     clock()
-    root.protocol("WM_DELETE_WINDOW", onClosing)
+    root.protocol("WM_DELETE_WINDOW", hide_C)
     root.mainloop()
 else:
-    writeWindowStatus('Open')
+    writeWindowStatus('Show')
